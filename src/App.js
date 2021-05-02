c1448a08eee90201cc31487f1c45b3ad03991806
Guillermo Lara <grlara9@gmail.com>
2021-05-01 19:33:07 -0700

    create a reducer and function to handle user submission

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React, {useReducer, useState}from 'react'
 
+const ACTIONS ={
 
+}
+
+const reducer= (state, action)=>{
+
+}
 
 function App() {
+
+  const [todos, dispatch] = useReducer(reducer, [])
+  const [name, setName] = useState('')
+
+
+  const handleSubmit =()=>{
+
+  }
+  
   return (
     <div className="App">
-      <form>
-        <input type="text" name="name" ></input>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange= {e => setName(e.target.value)}  />
         <button type="submit">Submit</button>
 
       
