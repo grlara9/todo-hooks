e123fb46a746fb7ae697f177d935cda8e5aa90dc
Guillermo Lara <grlara9@gmail.com>
2021-05-01 18:55:24 -0700

    initial cleaning react app and create form

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type="text" name="name" ></input>
+        <button type="submit">Submit</button>
+
+      
+      </form>
+    </div>
+  );
+}
+
+export default App;
