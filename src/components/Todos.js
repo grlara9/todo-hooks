a017eee785dbaa15ef9bd112908c44cf8e85e156
Guillermo Lara <grlara9@gmail.com>
2021-05-07 06:25:29 -0700

    enable user to toogle a todo once todo is done once is done will change color

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import {ACTIONS } from '../App'
-const Todos =({todo, dispatch, id, name})=> {
+const Todos =({todo, dispatch, id, name, complete})=> {
     return(
         <div>
+            <span style={{color : complete ? '#AAA' : '#000'}}>
+
             {name}
+            </span>
                 <button onClick={()=> dispatch({ type: ACTIONS.TOOGLE_TODO, payload:{id: id} })}> Completed </button>
 
                 <button onClick={()=> dispatch({ type: ACTIONS.DELETE_TODO, payload:{id: id} })}> Deleted </button>
