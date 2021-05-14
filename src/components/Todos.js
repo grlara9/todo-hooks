81e1e02a3d1f38803e72f1ba5e7fbae2cac510b8
Guillermo Lara <grlara9@gmail.com>
2021-05-13 23:01:08 -0700

    trying to figure it out how to add hover to icons

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -8,12 +8,17 @@ const Todos =({todo, dispatch, id, name, complete})=> {
     return(
         <div className="todo">
              <span style={{textDecoration : complete ? 'line-through'  : '', color: complete ? '#AAA': '#000'}}>{name}</span>
-            
-                <CheckCircleOutlineOutlinedIcon style={{color: 'green', float:'right', paddingLeft: '2px', fontSize:'1.2em'}} onClick={()=> dispatch({ type: ACTIONS.TOOGLE_TODO, payload:{id: id} })}  /> 
+                
+                <CheckCircleOutlineOutlinedIcon style={{color: 'green', float:'right', paddingLeft: '2px', fontSize:'1.2em', tr: {
+    background: "#f1f1f1",
+    '&:hover': {
+       background: "#f00",
+    },
+  },}} onClick={()=> dispatch({ type: ACTIONS.TOOGLE_TODO, payload:{id: id} })}  /> 
 
                 <HighlightOffOutlinedIcon style={{color: 'red', float:'right', fontSize:'1.2em'}}  onClick={()=> dispatch({ type: ACTIONS.DELETE_TODO, payload:{id: id} })} /> 
               
-            
+                
         </div>
     )
 }
